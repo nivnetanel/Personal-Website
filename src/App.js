fa46821dda50a2f167429fb2c2ecdbdbd196fa78
Niv Netanel <nivnetanel@gmail.com>
2022-03-18 14:53:04 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import logo from "./favicon.png";
-import "./App.css";
 import Home from "./components/home";
 import About from "./components/about";
 import Projectpage from "./components/projectpage";
 import Contact from "./components/contact";
-import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div>
-      = {/* <Navbar /> */}
+    <>
       <Home />
       <About />
       <Projectpage />
       <Contact />
-    </div>
+    </>
   );
 }
 
